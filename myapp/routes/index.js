b8d05c2f70ba620d6fe43444d5a9da1181474caf
saifrehman <saif.rehman1@ibm.com>
2018-02-03 04:03:57 +0400

    latest

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,95 +1,53 @@
 var express = require('express');
-var app = express.Router();
+var app = express();
 const Web3 = require('web3');
 
-
+app.set('view engine', 'ejs');
 /* GET home page. */
 app.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 
-
-app.get('/test', function (req, res, next) {
-  Web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
-  Web3.eth.defaultAccount = Web3.eth.accounts[0]
-  var loginAbi = require('../../build/contracts/Login.json').abi;
-  console.log('loginABI', loginAbi)
-  const LoginContract = Web3.eth.contract(loginAbi);
-  console.log('loginnnncontraaaaaact', LoginContract)
-  var loginContract = LoginContract.at('0x91dfecc6aa68f4e07c701284ebcb052f1f8845fc');
-  var loginAttempt = loginContract.LoginAttempt();
-  var challenges = {};
-  var successfulLogins = {};
-  console.log('loginAttempt', loginAttempt)
-  loginAttempt.watch((error, event) => {
-    console.log('in the eventtttttttttttttt')
-    if (error) {
-      console.log(error);
-      return;
-    }
-    console.log('event', event);
-    var sender = event.args.sender.toLowerCase();
-    if (challenges[sender] === event.args.challenge) {
-      successfulLogins[sender] = true;
-      res.send('respond with a resource');
-    }
-  });
+var jwt = require('jsonwebtoken');
+var ethUtil = require('ethereumjs-util');
+app.get('/hello', function(req, res) {
+  res.render('pages/hello');
 });
 
-
-app.post('/login', (req, res) => {
-  var jwt = require('jsonwebtoken');
-  // All Ethereum addresses are 42 characters long
-  if (!req.body.address || req.body.address.length !== 42) {
-    res.sendStatus(400);
-    return;
-  }
-  req.body.address = req.body.address.toLowerCase();
-  const challenge = ".7475443aA";
-  var challenges = {};
-  challenges[req.body.address] = challenge;
-  const token = jwt.sign({
-    address: req.body.address,
-    access: 'finishLogin'
-  }, "secret");
-  res.json({
-    challenge: challenge,
-    jwt: token
-  });
-});
-
-app.post('/finishLogin', (req, res) => {
-  if (!req.jwt || !req.jwt.address || req.jwt.access !== 'finishLogin') {
-    res.sendStatus(400);
-    return;
-  }
-  if (successfulLogins[req.jwt.address]) {
-    delete successfulLogins[req.jwt.address];
-    delete challenges[req.jwt.address];
-    const token = jwt.sign({
-      address: req.jwt.address,
-      access: 'full'
-    }, "secret");
-    res.json({
-      jwt: token,
-      address: req.jwt.address
-    });
+app.post('/works', (req, res) => {
+  var sig = req.body.sig;
+  var owner = req.body.owner;
+  owner = owner.toLowerCase();
+  // Same data as before
+  var data = req.body.data;
+  console.log(sig,owner,data);
+  var message = ethUtil.toBuffer(data)
+  var msgHash = ethUtil.hashPersonalMessage(message)
+  // Get the address of whoever signed this message  
+  var signature = ethUtil.toBuffer(sig)
+  var sigParams = ethUtil.fromRpcSig(signature)
+  var publicKey = ethUtil.ecrecover(msgHash, sigParams.v, sigParams.r, sigParams.s)
+  var sender = ethUtil.publicToAddress(publicKey)
+  var addr = ethUtil.bufferToHex(sender)
+ 
+  // Determine if it is the same address as 'owner' 
+  var match = false;
+  console.log("addr",addr);
+  console.log("owner",owner);
+  if (addr === owner) { match = true; }
+  if (match) {
+    var token = jwt.sign({user: req.body.addr}, 'secret',  { expiresIn: '1d' });
+    res.send(200, { success: 1, token: token })
   } else {
-    // HTTP Accepted (not completed)
-    res.sendStatus(202);
+    // If the signature doesn’t match, error out
+    res.send(500, { err: 'Signature did not match.'});
   }
 });
 
-app.post('/apiTest', (req, res) => {
-  if (req.jwt.access !== 'full') {
-    res.sendStatus(401); //Unauthorized
-    return;
-  }
-
-  res.json({
-    message: 'It works!'
-  });
-});
+function show(){
+  alert('clicked');
+  
+}
 
 module.exports = app;
